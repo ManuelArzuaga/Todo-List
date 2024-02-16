1446c05cbc2ac5a957a57f693fff84748f9700e9
arzuagamanuel <71405710+ManuelArzuaga@users.noreply.github.com>
2024-02-16 02:58:36 -0300

    commit-two

--- Backend.js
+++ Backend.js
@@ -27,13 +27,13 @@ let esquema = mongoose.Schema({ //se establece el esquema
 })
 const model = mongoose.model("Test2",esquema); //se usa para crear, eliminar,actualizar y leer
 
-
+//post tarea
 app.post("/",(req,res)=>{
   res.status = 200
   res.send({status:"recieved"})
   const newesquema = new model({//creo un nuevo esquema
     title:req.body.title,//se le asigna el req.body.titulo al nombre del esquema
-    // description:req.body.description
+    description:req.body.description
   })
 
   newesquema.save() //se guarda el esquema en la db
@@ -43,4 +43,46 @@ app.post("/",(req,res)=>{
     .catch((err) =>{
     console.log(err)
     })
+})
+
+//eliminar tarea
+
+app.delete("/",(req,res)=>{
+  res.status=200,
+  res.send({status:"deleted"})
+  model.deleteOne(req.body.title) //elimina por edad
+.then(()=>{
+  console.log("eliminado")
+})
+.catch((err)=>{
+  console.log(err)
+})
+})
+
+//actualizar tarea
+
+app.patch("/",(req,res)=>{
+  res.status=200,
+  res.send({status:"patched"})
+  model.findOneAndUpdate({title:req.body.title},{description:req.body.description}) //actualiza la edad buscando por el nombre, primera parte quien actualizar segunda parte que se actualiza
+.then(()=>{
+  console.log("actualizado")
+})
+.catch((err)=>{
+  console.log(err)
+})
+})
+
+//mostrar elementos
+
+app.get("/",(req,res)=>{
+  res.status=200
+  res.send("received")
+  model.find() //lee un elemento
+.then((users) =>{
+  console.log(users)
+})
+.catch((err) =>{
+  console.log(err)
+})
 })
